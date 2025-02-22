f7b65b310bf044c12cbfb554cebccd857a8f0e9f
rrosetta <taufik.m@kiranatama.com>
2025-02-22 13:19:37 +0700

    Update Draw.h

--- app/src/main/jni/Includes/Draw.h
+++ app/src/main/jni/Includes/Draw.h
@@ -42,7 +42,7 @@ public:
         return 0;
     }
 
-    void DrawLine(Unity::Color color, float thickness, Unity::Vector2 start, Unity::Vector2 end) {
+    void DrawLine(UnityResolve::UnityType::Color color, float thickness, UnityResolve::UnityType::Vector2 start, UnityResolve::UnityType::Vector2 end) {
         if (isValid()) {
             jclass canvasView = _env->GetObjectClass(_cvsView);
             jmethodID drawline = _env->GetMethodID(canvasView, "DrawLine",
@@ -50,47 +50,47 @@ public:
             _env->CallVoidMethod(_cvsView, drawline, _cvs, (int) color.a, (int) color.r,
                                  (int) color.g, (int) color.b,
                                  thickness,
-                                 start.X, start.Y, end.X, end.Y);
+                                 start.x, start.y, end.x, end.y);
         }
     }
 
-    void DrawText(Unity::Color color, const char *txt, Unity::Vector2 pos, float size) {
+    void DrawText(UnityResolve::UnityType::Color color, const char *txt, UnityResolve::UnityType::Vector2 pos, float size) {
         if (isValid()) {
             jclass canvasView = _env->GetObjectClass(_cvsView);
             jmethodID drawtext = _env->GetMethodID(canvasView, "DrawText",
                                                    "(Landroid/graphics/Canvas;IIIILjava/lang/String;FFF)V");
             _env->CallVoidMethod(_cvsView, drawtext, _cvs, (int) color.a, (int) color.r,
                                  (int) color.g, (int) color.b,
-                                 _env->NewStringUTF(txt), pos.X, pos.Y, size);
+                                 _env->NewStringUTF(txt), pos.x, pos.y, size);
         }
     }
 
     // public void DrawCircle(Canvas cvs, int a, int r, int g, int b, float stroke, float posX, float posY, float radius) {
-    void DrawCircle(Unity::Color color, float stroke, Unity::Vector2 pos, float radius) {
+    void DrawCircle(UnityResolve::UnityType::Color color, float stroke, UnityResolve::UnityType::Vector2 pos, float radius) {
         if (isValid()) {
             jclass canvasView = _env->GetObjectClass(_cvsView);
             jmethodID drawcircle = _env->GetMethodID(canvasView, "DrawCircle",
                                                      "(Landroid/graphics/Canvas;IIIIFFFF)V");
             _env->CallVoidMethod(_cvsView, drawcircle, _cvs, (int) color.a, (int) color.r,
-                                 (int) color.g, (int) color.b, stroke, pos.X, pos.Y, radius);
+                                 (int) color.g, (int) color.b, stroke, pos.x, pos.y, radius);
         }
     }
 
-    void DrawFilledCircle(Unity::Color color, Unity::Vector2 pos, float radius) {
+    void DrawFilledCircle(UnityResolve::UnityType::Color color, UnityResolve::UnityType::Vector2 pos, float radius) {
         if (isValid()) {
             jclass canvasView = _env->GetObjectClass(_cvsView);
             jmethodID drawfilledcircle = _env->GetMethodID(canvasView, "DrawFilledCircle",
                                                            "(Landroid/graphics/Canvas;IIIIFFF)V");
             _env->CallVoidMethod(_cvsView, drawfilledcircle, _cvs, (int) color.a, (int) color.r,
-                                 (int) color.g, (int) color.b, pos.X, pos.Y, radius);
+                                 (int) color.g, (int) color.b, pos.x, pos.y, radius);
         }
     }
 
-    void DrawBox(Unity::Color color, float stroke, Unity::Rect rect) {
-        Unity::Vector2 v1 = Unity::Vector2(rect.fX, rect.fY);
-        Unity::Vector2 v2 = Unity::Vector2(rect.fX + rect.fWidth, rect.fY);
-        Unity::Vector2 v3 = Unity::Vector2(rect.fX + rect.fWidth, rect.fY + rect.fHeight);
-        Unity::Vector2 v4 = Unity::Vector2(rect.fX, rect.fY + rect.fHeight);
+    void DrawBox(UnityResolve::UnityType::Color color, float stroke, UnityResolve::UnityType::Rect rect) {
+        UnityResolve::UnityType::Vector2 v1 = UnityResolve::UnityType::Vector2(rect.fX, rect.fY);
+        UnityResolve::UnityType::Vector2 v2 = UnityResolve::UnityType::Vector2(rect.fX + rect.fWidth, rect.fY);
+        UnityResolve::UnityType::Vector2 v3 = UnityResolve::UnityType::Vector2(rect.fX + rect.fWidth, rect.fY + rect.fHeight);
+        UnityResolve::UnityType::Vector2 v4 = UnityResolve::UnityType::Vector2(rect.fX, rect.fY + rect.fHeight);
 
         DrawLine(color, stroke, v1, v2);
         DrawLine(color, stroke, v2, v3);
@@ -98,26 +98,26 @@ public:
         DrawLine(color, stroke, v4, v1);
     }
 
-    void DrawHorizontalHealthBar(Unity::Vector2 screenPos, float width, float maxHealth, float currentHealth) {
-        screenPos -= Unity::Vector2(0.0f, 8.0f);
-        DrawBox(Unity::Color(0, 0, 0, 255), 3, Unity::Rect(screenPos.X, screenPos.Y, width + 2, 5.0f));
-        screenPos += Unity::Vector2(1.0f, 1.0f);
-        Unity::Color clr = Unity::Color(0, 255, 0, 255);
+    void DrawHorizontalHealthBar(UnityResolve::UnityType::Vector2 screenPos, float width, float maxHealth, float currentHealth) {
+        screenPos -= UnityResolve::UnityType::Vector2(0.0f, 8.0f);
+        DrawBox(UnityResolve::UnityType::Color(0, 0, 0, 255), 3, UnityResolve::UnityType::Rect(screenPos.x, screenPos.y, width + 2, 5.0f));
+        screenPos += UnityResolve::UnityType::Vector2(1.0f, 1.0f);
+        UnityResolve::UnityType::Color clr = UnityResolve::UnityType::Color(0, 255, 0, 255);
         float hpWidth = (currentHealth * width) / maxHealth;
         if (currentHealth <= (maxHealth * 0.6)) {
-            clr = Unity::Color(255, 255, 0, 255);
+            clr = UnityResolve::UnityType::Color(255, 255, 0, 255);
         }
         if (currentHealth < (maxHealth * 0.3)) {
-            clr = Unity::Color(255, 0, 0, 255);
+            clr = UnityResolve::UnityType::Color(255, 0, 0, 255);
         }
-        DrawBox(clr, 3, Unity::Rect(screenPos.X, screenPos.Y, hpWidth, 3.0f));
+        DrawBox(clr, 3, UnityResolve::UnityType::Rect(screenPos.x, screenPos.y, hpWidth, 3.0f));
     }
 
-    void DrawCrosshair(Unity::Color clr, Unity::Vector2 center, int size = 20) {
-        float x = center.X - (size / 2.0f);
-        float y = center.Y - (size / 2.0f);
-        DrawLine(clr, 3, Unity::Vector2(x, center.Y), Unity::Vector2(x + size, center.Y));
-        DrawLine(clr, 3, Unity::Vector2(center.X, y), Unity::Vector2(center.X, y + size));
+    void DrawCrosshair(UnityResolve::UnityType::Color clr, UnityResolve::UnityType::Vector2 center, int size = 20) {
+        float x = center.x - (size / 2.0f);
+        float y = center.y - (size / 2.0f);
+        DrawLine(clr, 3, UnityResolve::UnityType::Vector2(x, center.y), UnityResolve::UnityType::Vector2(x + size, center.y));
+        DrawLine(clr, 3, UnityResolve::UnityType::Vector2(center.x, y), UnityResolve::UnityType::Vector2(center.x, y + size));
     }
 };
 
